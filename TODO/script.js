e6af2baf2f66f6221e9601421007975a67ec2ae7
Victor Emanoel <ve793221@gmail.com>
2025-11-27 10:29:45 -0300

    exercicio TODO

---
+++ TODO/script.js
@@ -0,0 +1,20 @@
+document.getElementById("todo-form")
+document.getElementById("task-input")
+document.getElementById("todo-list")
+
+let form = document.getElementById("todo-form")
+let input = document.getElementById("task-input")
+let list = document.getElementById("todo-list")
+
+function addTask(event) {
+    event.preventDefault();
+    let valor = input.value
+    if (valor !== ''){
+        let novoitem = document.createElement(li)
+    }
+
+}
+form.addEventListener("Submit", "addtask")
+document.createElement()
+
+
